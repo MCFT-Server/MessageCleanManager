90e54fd917d0f52f6f06af0af3bda1c32ea515ee
maru <wsj7178@gmail.com>
2016-10-30 12:53:22 +0900

    add sign option

--- src/messagecleanmanager/Main.java
+++ src/messagecleanmanager/Main.java
@@ -3,6 +3,7 @@ package messagecleanmanager;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.block.SignChangeEvent;
 import cn.nukkit.event.player.PlayerChatEvent;
 import cn.nukkit.event.player.PlayerCommandPreprocessEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
@@ -13,25 +14,41 @@ public class Main extends PluginBase implements Listener {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
-		saveDefaultConfig();
+		initConfig();
 	}
 	
+	private void initConfig() {
+		saveDefaultConfig();
+		if (getConfig().get("clean-sign") == null) {
+			saveResource("config.yml", true);
+		}
+	}
+
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
 		event.getPlayer().setRemoveFormat(false);
 	}
-	
+
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onCommand(PlayerCommandPreprocessEvent event) {
-		if (getConfig().getBoolean("clean-command")) {
+		if (getConfig().getBoolean("clean-command") && !event.getPlayer().isOp()) {
 			event.setMessage(TextFormat.clean(event.getMessage()));
 		}
 	}
-	
+
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onChat(PlayerChatEvent event) {
-		if (getConfig().getBoolean("clean-chat")) {
+		if (getConfig().getBoolean("clean-chat") && !event.getPlayer().isOp()) {
 			event.setMessage(TextFormat.clean(event.getMessage()));
+		}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onSignChange(SignChangeEvent event) {
+		if (getConfig().getBoolean("clean-sign") && !event.getPlayer().isOp()) {
+			for (int i = 0; i < 4; i++) {
+				event.setLine(i, TextFormat.clean(event.getLine(i)));
+			}
 		}
 	}
 }
